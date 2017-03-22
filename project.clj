bd7cf7a23b404401b08424cd1bb1d87e70dd59dd
Buck Ryan <buck.ryan@gmail.com>
2017-03-21 22:38:53 -0400

    version 0.7.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject com.buckryan/farmhand-benchmark "0.1.0-SNAPSHOT"
   :url "https://github.com/b-ryan/farmhand-benchmark"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.buckryan/farmhand "0.7.0-SNAPSHOT"]
+                 [com.buckryan/farmhand "0.7.0"]
                  [log4j "1.2.17"]]
   :main ^:skip-aot farmhand.benchmark.core
   :target-path "target/%s"
